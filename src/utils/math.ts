74c6b046394395034da47deda2ce445ee28774e8
AlanBlanchet <alan@alan-blanchet.fr>
2021-11-18 19:30:26 +0100

    [DEV]Added bound function

---
+++ src/utils/math.ts
@@ -0,0 +1,21 @@
+/**
+ * The function takes a number and returns another number
+ * that is inside the bound
+ * @param n The number that needs to be bound
+ * @param bound The number that bounds [-bound, bound]
+ */
+function bound(n: number, bound: number): number;
+
+/**
+ * The function takes a number and returns another number
+ * that is inside the bound
+ * @param n The number that needs to be bound
+ * @param min The lowest number that n can be
+ * @param max The highest number that n can be
+ */
+function bound(n: number, min: number, max: number): number;
+
+function bound(n: number, min: number, max?: number) {
+  const minMax = max ?? -min;
+  return n > minMax ? minMax : n < min ? min : n;
+}
