5723d2620a19042e14e069376d1c71d1c8ab1106
AlanBlanchet <alan@alan-blanchet.fr>
2021-11-18 19:30:56 +0100

    [DEV][DOC]Added type documentation and event
    Added a click event and some documentation to understand the functions

--- src/utils/react.ts
+++ src/utils/react.ts
@@ -6,6 +6,11 @@ import {
   useState,
 } from 'react';
 
+/**
+ * Combination of useState and useRef
+ * @param initialState The initial state
+ * @returns The reference and the setState function
+ */
 export const useStateRef = <T extends unknown>(
   initialState: T | (() => T)
 ): [MutableRefObject<T>, (data: T) => void] => {
@@ -23,20 +28,26 @@ export const useStateRef = <T extends unknown>(
   ];
 };
 
-const usePeripheral = (
+/**
+ * Allows to use a document event listener
+ * @param type The type of DOM event
+ * @param f The document callback
+ * @param deps The dependency list
+ */
+export const useEvent = (
   type: keyof DocumentEventMap,
   f: (this: Document, ev: DocumentEventMap[keyof DocumentEventMap]) => any,
   deps?: DependencyList
 ) => {
   useEffect(() => {
     document.addEventListener(type, f);
-
     return () => {
       document.removeEventListener(type, f);
     };
   }, deps ?? []);
 };
 
+type KeyType = 'down' | 'press' | 'up';
 type MouseType =
   | 'down'
   | 'enter'
@@ -46,27 +57,40 @@ type MouseType =
   | 'over'
   | 'up'
   | 'click';
-type KeyType = 'down' | 'press' | 'up';
 
-export const useMouse = (
-  type: MouseType,
-  f: (e: MouseEvent) => any,
-  deps?: DependencyList
-) => {
-  usePeripheral(
-    type == 'click' ? type : `mouse${type}`,
-    f as (this: Document, ev: DocumentEventMap[keyof DocumentEventMap]) => any,
-    deps
-  );
-};
-
+/**
+ * A subset of useEffect but with the global
+ * document event listener
+ * @param type The type of key event
+ * @param f The document callback
+ * @param deps The dependency list
+ */
 export const useKey = (
   type: KeyType,
   f: (this: Document, ev: KeyboardEvent) => any,
   deps?: DependencyList
 ) => {
-  usePeripheral(
+  useEvent(
     `key${type}`,
+    f as (this: Document, ev: DocumentEventMap[keyof DocumentEventMap]) => any,
+    deps
+  );
+};
+
+/**
+ * A subset of useEffect but with the global
+ * document event listener
+ * @param type The type of mouse event
+ * @param f The document callback
+ * @param deps The dependency list
+ */
+export const useMouse = (
+  type: MouseType,
+  f: (e: MouseEvent) => any,
+  deps?: DependencyList
+) => {
+  useEvent(
+    type == 'click' ? type : `mouse${type}`,
     f as (this: Document, ev: DocumentEventMap[keyof DocumentEventMap]) => any,
     deps
   );
