2766635d97b63d3921449430099613f00d545732
AlanBlanchet <alan@alan-blanchet.fr>
2021-11-21 21:47:05 +0100

    [FIX]Added other exports

--- src/module.ts
+++ src/module.ts
@@ -1,4 +1,5 @@
-export { useKey, useMouse } from 'utils/react';
-export { bound } from 'utils/math';
+export { useKey, useMouse, useContextMenu, useEvent, Click } from 'utils/react';
 export { useStateRef } from 'utils/state/ref';
 export { useStateArray, StateArray } from 'utils/state/array';
+export { bound } from 'utils/math';
+export { FixedLengthArray, NumberRange, Offset, Position } from 'utils/types';
